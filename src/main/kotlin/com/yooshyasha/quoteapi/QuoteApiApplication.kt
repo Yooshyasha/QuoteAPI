ac32c6175fd45818d490dfbf21f788caaf54d4ee
Yooshyasha <mejzikrejzi66@gmail.com>
2024-11-03 11:04:48 +0500

    Initial Commit

---
+++ src/main/kotlin/com/yooshyasha/quoteapi/QuoteApiApplication.kt
@@ -0,0 +1,11 @@
+package com.yooshyasha.quoteapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class QuoteApiApplication
+
+fun main(args: Array<String>) {
+    runApplication<QuoteApiApplication>(*args)
+}
